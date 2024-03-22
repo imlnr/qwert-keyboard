d67b799d36986846938c039e5797afdd84d24f57
imlnr <imlnr2002@gmail.com>
2024-03-22 12:57:11 +0530

    working on js

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const keyboardButtons = document.querySelectorAll('.keyboard div');
+    let caps = false;
+
+    keyboardButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const textArea = document.querySelector('.text-area');
+            console.log(button.textContent);
+            if (button.textContent === 'delete') {
+                textArea.textContent = textArea.textContent.slice(0, -1);
+            }
+            else if (button.textContent === 'Space') {
+                textArea.textContent += ' ';
+            }
+            else if (button.textContent === 'Caps ') {
+                caps = !caps;
+                const capsNoti = document.querySelector('.caps-noti');
+                capsNoti.style.color = caps ? 'red' : 'white';
+                console.log(caps);
+            }
+            else {
+                if (caps) {
+                    textArea.textContent += button.textContent.toUpperCase();
+                } else {
+                    textArea.textContent += button.textContent.toLowerCase();
+                }
+            }
+        });
+    });
+});
